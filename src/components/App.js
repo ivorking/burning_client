6b582a93b6251326118051ee7028ba7200a276cb
Ivor King <ivorking@gmail.com>
2018-04-18 12:41:34 +1000

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,9 @@ class App extends Component {
 
       <div className="App">
 
-        <ul>
-          <li><a href="/airplanes">Airplanes</a></li>
-          <li><a href="/flights">Flights</a></li>
-          <li><a href="/users">Users</a></li>
-          <li><a href="/reservations">Reservations</a></li>
-        </ul>
-
         <h1>Burning Airlines</h1>
         <Airplanes />
-        <Flights />
+
       </div>
     );
   }
--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -4,23 +4,28 @@ import PropTypes from 'prop-types';
 class Reservation extends Component {
     constructor(props) {
         super(props);
+        this._handleChange = this._handleChange.bind(this); //check react console secrets form state.
+        this._handleSubmit = this._handleSubmit.bind(this);
     }
 
     _handleInput(e) {
         this.setState( {query: e.target.value} );
       }
     
-      _handleSubmit(e) {
-        e.preventDefault();
-        this.props.onSubmit( this.state.query );
-      }
+    _handleSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.content);
+    this.setState({content: ''});
+    }
 
     render () {
         return (
             <div className="reservation"/>
             <form onSubmit={ this._handleSubmit }>
-                <input type="search" placeholder="Butterfly" onInput={ this._handleInput }/>
-                <input type="submit" value="Search" />
+                <input type="date" onChange={this._handleChange} value={this.state.content} />
+                <input type="text" placeholder="Origin" onChange={this._handleChange} value={this.state.content} />
+                <input type="text" placeholder="Destination" onChange={this._handleChange} value={this.state.content} />
+                <input type="submit" value="Create Flight" />
             </form>
         );
     }
