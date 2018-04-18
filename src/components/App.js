dee3132e018abede6db6bdb8786c1f3392cc816e
Ricky <ricky@Rickys-MacBook-Pro.local>
2018-04-18 12:40:37 +1000

    Fixed most routes, removed route for Reservations

--- src/components/App.js
+++ src/components/App.js
@@ -8,17 +8,9 @@ class App extends Component {
     return (
 
       <div className="App">
-
-        <ul>
-          <li><a href="/airplanes">Airplanes</a></li>
-          <li><a href="/flights">Flights</a></li>
-          <li><a href="/users">Users</a></li>
-          <li><a href="/reservations">Reservations</a></li>
-        </ul>
-
         <h1>Burning Airlines</h1>
         <Airplanes />
-        <Flights />
+        
       </div>
     );
   }
