9980f09a08561f16568e7c0495830d8210cf21ea
Ricky <ricky@Rickys-MacBook-Pro.local>
2018-04-18 10:37:57 +1000

    Created Airplanes.js

---
+++ src/components/Airplanes.js
@@ -0,0 +1,17 @@
+import React, { PureComponent as Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+//const AIRPLANES_URL = 'http://localhost:3000/airplanes.json';
+
+class AirplanesForm extends Component {
+  render(){
+    return(
+
+        <h2>Airplanes</h2>
+
+    );
+  }
+}
+
+export default AirplanesForm;
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Airplanes from './Airplanes';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>Burning Airlines</h1>
+        <Airplanes />
       </div>
     );
   }
