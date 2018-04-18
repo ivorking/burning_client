cf4335c55744a35e9861c0b2a08f2f595968acd4
Ricky <ricky@Rickys-MacBook-Pro.local>
2018-04-18 16:19:15 +1000

    Airplanes

--- src/components/Airplanes.js
+++ src/components/Airplanes.js
@@ -1,27 +1,77 @@
 import React, { PureComponent as Component } from 'react';
 import PropTypes from 'prop-types';
 
-//const AIRPLANES_URL = 'http://localhost:3000/airplanes.json';
+import axios from 'axios';
+
+const AIRPLANES_URL = 'http://localhost:3000/airplanes.json';
 
 
-class AirplaneForm extends Component {
+
+class CreateAirplaneForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '',
+                   rows: '',
+                   columns: ''};
+    this._handleChange = this._handleChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleChange(e){
+    // this.setState( { name: e.target.value,
+    //                  rows: e.target.value,
+    //                  columns: e.target.columns} );
+    console.log(e.target.name);
+  }
+
+  _handleSubmit(e){
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    // debugger;
+    this.setState({name: '', rows: '', columns: ''});
+  }
+
   render(){
     return (
-
-      <h3>Airplane Form </h3>
-    )
-  }
-}
-
-class AirplanesForm extends Component {
-  render(){
-    return(
       <div>
-        <h2>Airplanes</h2>
-        <AirplaneForm />
+        <h3>Create new Airplane</h3>
+        <form onSubmit = {this._handleSubmit}>
+          <label>Name:</label>
+          <input type="text"
+                 name="name"
+                 onChange = {this._handleChange}></input>
+
+          <label>Rows:</label>
+          <input type="number"
+                 name="rows"
+                 onChange = {this._handleChange}></input>
+
+          <label>Columns:</label>
+          <input type="number"
+                 name="columns"
+                 onChange = {this._handleChange}></input>
+
+          <input type="submit" value="Create"/>
+        </form>
       </div>
     );
   }
 }
 
-export default AirplaneForm;
+class Airplanes extends Component {
+
+  createAirplane(query){
+    console.log('Creating: ', query);
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>Airplanes</h2>
+        <CreateAirplaneForm onSubmit = {this.createAirplane}/>
+      </div>
+    );
+  }
+}
+
+export default Airplanes;
