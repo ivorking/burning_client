da07c92aca99ad8968dd7e5a865e19b757484e28
Ivor King <ivorking@gmail.com>
2018-04-18 16:24:45 +1000

    updates

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Airplanes from './components/Airplanes';
 import Flights from './components/Flights';
+import Reservations from './components/Reservations'
 
 const Routes = (
   <Router>
@@ -11,6 +12,7 @@ const Routes = (
       <Route exact path = "/" component={ Home } />
       <Route exact path = "/Airplanes" component = { Airplanes } />
       <Route exact path = "/Flights" component = { Flights } />
+      <Route exact path = "/Reservations" component = { Reservations } />
     </div>
   </Router>
 );
