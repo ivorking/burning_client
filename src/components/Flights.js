45942e3772f5a8e8dcbd6bdf410b4d20f27c4ade
Vu <vutple@gmail.com>
2018-04-19 14:00:35 +1000

    final push

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -1,6 +1,7 @@
 import React, { PureComponent as Component } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 
 
 
@@ -64,13 +65,10 @@ function Gallery (props) {
 
     <div>
     <h2>All Flights</h2>
-    { props.flights.map( (f) => <p key={ f.id }>{ f.id } { f.flightTime } { f.source } { f.destination } {f.airplane_id}</p> ) }
+    { props.flights.map( (f) => <p key={ f.id }>{ f.flightTime } { f.source } { f.destination } {f.airplane_id} <button><Link to={`/reservations/`}>Reservations</Link></button></p> ) }
     </div>
   )
 }
-
-
-
 
 class Flights extends Component {
   constructor(props) {
@@ -103,5 +101,64 @@ class Flights extends Component {
     );
   }
 }
+
+// class SearchFlightForm extends Component {
+//   constructor(props) {
+//       super(props);
+//       this.state = { flightTime: '',
+//                       source: '',
+//                       destination: ''
+//     };
+//       this._handleFlightTimeChange = this._handleFlightTimeChange.bind(this);
+//       this._handleSourceChange = this._handleSourceChange.bind(this);
+//       this._handleDestinationChange = this._handleDestinationChange.bind(this);
+//       this._handleSubmit = this._handleSubmit.bind(this);
+//     }
+//     _handleFlightTimeChange(e) {
+//     this.setState({flightTime: e.target.value});
+//   }
+//
+//   _handleSourceChange(e) {
+//     this.setState({source: e.target.value});
+//   }
+//
+//   _handleDestinationChange(e) {
+//     this.setState({destination: e.target.value});
+//   }
+//
+//   _handleSubmit(e) {
+//     e.preventDefault();
+//     this.props.onSubmit(this.state.flightTime, this.state.source, this.state.destination);
+//     this.setState({ flightTime: '', source: '', destination: ''})
+//   }
+//
+// render() {
+//     return (
+//       <div>
+//         <h2>Search Flights</h2>
+//         <form onSubmit={this._handleSubmit}>
+//           <input type="date" onChange={this._handleFlightTimeChange} value={this.state.flightTime} />
+//           <input type="text" placeholder="Origin" onChange={this._handleSourceChange} value={this.state.source} />
+//           <input type="text" placeholder="Destination" onChange={this._handleDestinationChange} value={this.state.destination} />
+//           <input type="submit" value="Search" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+//
+// SearchFlightForm.propTypes = {
+//   onSubmit: PropTypes.func.isRequired
+// }
+//
+// function Gallery (props) {
+//   return (
+//
+//     <div>
+//     <h2>All Flights</h2>
+//     { props.flights.map( (f) => <p key={ f.id }>{ f.id } { f.flightTime } { f.source } { f.destination } {f.airplane_id}</p> ) }
+//     </div>
+//   )
+// }
 
 export default Flights;
